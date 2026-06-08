4538aeb8397547fa2f8fc583152ba2f9d578d0d5
Lilian Aguilar <lilian.aguilar@galileo.edu>
2026-06-07 22:06:46 -0600

    upload files

---
+++ infra/envs/dev/backend-dev.hcl
@@ -0,0 +1,3 @@
+bucket = "ex-7-2-bucket"
+key    = "exercise-7-2/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ infra/main.tf
@@ -0,0 +1,5 @@
+
+resource "aws_sqs_queue" "main" {
+  name                       = var.queue_name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+}
